441f52c510bb94117db2a9a8f1ae5dac1fa20968
aureliacitra <citrapangukir@gmail.com>
2025-04-28 19:17:28 +0700

    add library

--- contoh2.cpp
+++ contoh2.cpp
@@ -1,34 +1,3 @@
 #include <iostream>
 using namespace std;
 
-class persegipanjang{
-      public:
-      float panjang;
-      float lebar;
-      float luas;
-
-      void inputdata(){
-        cout << "input panjang : ";
-        cin >> panjang;
-        cout << "input lebar : ";
-        cin >> lebar;
-      }
-      
-      void menghitungluas(){
-        luas = panjang*lebar;
-      }
-
-      void output(){
-        cout << "luas persegi panjang = " << luas << endl;
-      }
-};
-
-
-int main(){
-    persegipanjang  pj;
-    pj.inputdata();
-    pj.menghitungluas();
-    pj.output();
-    return 0;
-
-}
